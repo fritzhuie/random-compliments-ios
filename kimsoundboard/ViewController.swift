49063c05fd8b8a7a4c31eb2da776912b279c5c3a
Fritz Huie <fritzhuie@gmail.com>
2016-07-06 19:53:41 -0700

    initital commit

---
+++ kimsoundboard/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  kimsoundboard
+//
+//  Created by Fritz Huie on 7/6/16.
+//  Copyright © 2016 Fritz. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        for each in allSounds.keys {
+            remainingSounds.append(each)
+        }
+    }
+    
+    var audioPlayer = AVAudioPlayer()
+    
+    @IBAction func buttonTapped(sender: UIButton) {
+        
+        if(remainingSounds.count < 1){
+            for each in allSounds.keys {
+                remainingSounds.append(each)
+            }
+        }
+        
+        let itr = Int(arc4random_uniform(UInt32(allSounds.count)))
+        let soundName = remainingSounds[itr]
+        remainingSounds.removeAtIndex(itr)
+        
+        let url = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource(soundName, ofType:nil)!)
+        audioPlayer = try! AVAudioPlayer(contentsOfURL: url)
+        audioPlayer.play()
+    }
+    
+    var remainingSounds = [String]()
+    var allSounds:[String:String?] = [
+        "beautiful.mp3":"emily.jpg",
+        "awesome.mp3":nil,]
+
+}
+
