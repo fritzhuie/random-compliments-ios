d65d4b0f8fbd2c7831874847629587dc40b2d06c
Fritz Huie <fritzhuie@gmail.com>
2016-07-13 22:21:33 -0700

    updated

--- kimsoundboard/ViewController.swift
+++ kimsoundboard/ViewController.swift
@@ -15,10 +15,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         label.frame = self.view.frame
-        label.font = label.font.fontWithSize(40)
+        label.font = UIFont(name: "arial", size: 40)
         label.textAlignment = .Center;
+        label.textColor = UIColor.blueColor()
         self.view.addSubview(label)
-        
+        bgview.alpha = 0.0
         for each in allSounds.keys {
             remainingSounds.append(each)
         }
@@ -28,16 +29,28 @@ class ViewController: UIViewController {
     
     func textPopup (string:String?) {
         label.text = string
-        UIView.animateWithDuration(1.0, delay: 0.0, options: UIViewAnimationOptions.CurveEaseIn, animations: {
+        UIView.animateWithDuration(2.0, delay: 0.0, options: UIViewAnimationOptions.CurveEaseIn, animations: {
             self.label.alpha = 1.0
-            }, completion: nil)
-        
-        UIView.animateWithDuration(2.0, delay: 1.0, options: UIViewAnimationOptions.CurveEaseOut, animations: {
-            self.label.alpha = 0.0
+            self.label.transform = CGAffineTransformMakeScale(2, 2)
+            self.label.transform = CGAffineTransformIdentity
+            self.button.alpha = 0.0
+            self.bgview.alpha = 1.0
+            
             }, completion: {
                 (finished: Bool) -> Void in
+                UIView.animateWithDuration(1.0, delay: 0.0, options: UIViewAnimationOptions.CurveEaseOut, animations: {
+                    self.label.alpha = 0.0
+                    self.label.transform = CGAffineTransformMakeScale(6, 6)
+                    self.label.transform = CGAffineTransformMakeTranslation(-256, -256)
+                    self.label.transform = CGAffineTransformMakeRotation(CGFloat(M_PI))
+                    self.label.transform = CGAffineTransformIdentity
+                    self.bgview.alpha = 0.0
+                    self.button.alpha = 1.0
+                    }, completion: {
+                        (finished: Bool) -> Void in
+                })
         })
-                
+        
     }
     
     
@@ -45,10 +58,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var button: UIButton!
     
+    @IBOutlet weak var bgview: UIImageView!
     @IBAction func buttonTapped(sender: UIButton) {
-        
-        button.enabled = false
-        _ = NSTimer.scheduledTimerWithTimeInterval(2.0, target: self, selector: #selector(self.enableButton), userInfo: nil, repeats: false)
         
         var soundName:String = remainingSounds[0]
         
@@ -59,12 +70,10 @@ class ViewController: UIViewController {
                 remainingSounds.append(each)
             }
         }else{
-            let itr = Int(arc4random_uniform(UInt32(allSounds.count)))
+            let itr = Int(arc4random_uniform(UInt32(remainingSounds.count)))
             soundName = remainingSounds[itr]
             remainingSounds.removeAtIndex(itr)
         }
-        
-        
         
         textPopup(allSounds[soundName]!)
         
@@ -79,8 +88,21 @@ class ViewController: UIViewController {
     
     var remainingSounds = [String]()
     var allSounds:[String:String?] = [
-        "beautiful.mp3":"You are beautiful!",
-        "awesome.mp3":"You are awesome!",]
+        "affection.mp3":"You are beautiful!",
+        "assholes.mp3":"You are awesome!",
+        "bedhead.mp3":"You are beautiful!",
+        "burgers.mp3":"You are awesome!",
+        "fabulous.mp3":"You are beautiful!",
+        "fuckingbeautiful.mp3":"You are awesome!",
+        "killingit.mp3":"You are beautiful!",
+        "laugh.mp3":"You are awesome!",
+        "loved.mp3":"You are beautiful!",
+        "opinions.mp3":"You are awesome!",
+        "proud.mp3":"You are beautiful!",
+        "shittyday.mp3":"You are awesome!",
+        "thelight.mp3":"You are beautiful!",
+        "warmestperson.mp3":"You are awesome!",
+        "wisdom.mp3":"You are beautiful!"]
 
 }
 
